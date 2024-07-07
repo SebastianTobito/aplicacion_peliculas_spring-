2b753e6f4b97fbc2483769b84aa28cf76e42c74b
Sebastian Tobito <sebastiantobito@gmail.com>
2024-07-07 13:57:51 -0500

    creando la clase consumoApi para traer datos de la api

---
+++ src/main/java/com/curso/screenfilme/ScreenFilmeApplication.java
@@ -0,0 +1,24 @@
+package com.curso.screenfilme;
+
+import com.curso.screenfilme.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenFilmeApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenFilmeApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		//var json = consumoApi.obtenerDatos("http://www.omdbapi.com/?t=dragon+ball&apikey=e212bfb");
+		var json = consumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+
+	}
+}
---
+++ src/test/java/com/curso/screenfilme/ScreenFilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.screenfilme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenFilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
